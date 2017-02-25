ef151ce7cd6cd83e40a0d15cd5cf83b9edf3b598
David Couzelis <drcouzelis@gmail.com>
2017-02-24 22:11:23 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,49 @@
+# SMS Backup+ Backup
+
+*David Couzelis, 2017*
+
+## Background
+
+There is an outstanding open source application for Android devices called "SMS Backup+" that will automatically upload your SMS (and MMS) to an IMAP server, such as Gmail. See here for more information:
+
+**SMS Backup+**, *Jan Berkel*, https://github.com/jberkel/sms-backup-plus
+
+In addition to having a copy saved online, I also want to be able to have a nicely formatted local copy of my messages (text files, pictures...). That's where this software comes in...
+
+## Purpose
+
+This software allows you to save the messages that were backed up to Gmail by SMS Backup+ as nicely formatted text files on your computer, including an MMS pictures and videos you received.
+
+## Usage
+
+Go to the Gmail website to download an archive of your messages. Be sure to only select the message that were uploaded by SMS Backup+ and that you want saved.
+
+    https://takeout.google.com/settings/takeout
+
+Extract the mbox file from the download and send it to the function provided in this software.
+
+See the Python docstring for more information on usage.
+
+## Example output
+
+The filenames created will look like this:
+
+    20160921-IMG_8904.jpg
+    20160921-IMG_8907.jpg
+    20161003-497209663.jpg
+    20161004-2f033013c.gif
+    20161008-IMG_9290.3gp
+    sms-201609-20170224.txt
+    sms-201610-20170224.txt
+    sms-201611-20170224.txt
+    sms-201612-20170224.txt
+    sms-201701-20170224.txt
+    sms-201702-20170224.txt
+
+And the text files will be formatted like this:
+
+    John (2017-02-05 13:56:27): We are driving to Disneyland right now.
+    John (2017-02-05 13:57:15): [IMG_0411.3gp]
+    John (2017-02-05 13:58:06): [508013861.jpg]
+    Tony (2017-02-05 14:09:47): Oh my gosh!!! It was a surprise to me too! Lol 😄
+
---
+++ smsbackupbackup.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+
+# smsbackupbackup
+# David Couzelis, 2017
+
+import mailbox
+import datetime
+import sys
+
+def run(mbox_filename, my_name, my_descriptor_list, friend_name, friend_descriptor_list):
+    """Read an mbox mailbox made from a downloaded archive of SMS Backup+ messages and
+    save them to local text files. Any MMS pictures or videos that are received (but
+    not those that you sent) will be saved as well.
+
+    mbox_files             : The filename of an mbox mailbox archive
+    my_name                : A string containing your name
+    my_descriptor_list     : A list of strings that are something in the 'From' field
+                             of the downloaded email denoting you (your name, phone
+                             number...)
+    friend_name            : A string containing the name of your contact
+    friend_descriptor_list : A list of strings, likewise, for the contact
+
+    The messages and media will be formatted nicely and saved in text documents
+    by year + month.
+    """
+
+    try:
+        mbox = mailbox.mbox(mbox_filename, create=False)
+    except:
+        print('Failed to load mbox file: ' + mbox_filename, file=sys.stderr)
+        exit(1)
+    
+    # A timestamp (YYYYMMDD), to be used as a suffix specifying when the output files were created
+    timestamp_str = datetime.datetime.now().strftime('%Y%m%d')
+    
+    # Use the message 'Sent' date to sort the messages
+    messages = dict()
+    for key in mbox.keys():
+        datetime_str = datetime.datetime.strptime(mbox[key]['Date'], '%a, %d %b %Y %H:%M:%S %z')
+        messages[datetime_str] = mbox[key]
+    
+    for datetime_str in sorted(messages):
+    
+        from_me = True
+    
+        message = messages[datetime_str]
+        sms_str = str()
+    
+        # Begin the line of output with who sent the message...
+        if any(x in message['From'] for x in friend_descriptor_list):
+            # This message was sent by the friend
+            sms_str += friend_name + ' '
+            from_me = False
+        elif any(x in message['From'] for x in my_descriptor_list):
+            # This message was sent by me
+            sms_str += my_name + ' '
+        else:
+            # I have no clue who sent this message!
+            print('WARNING: Unknown sender: ' + message['From'], file=sys.stderr)
+            sms_str += message['From']
+    
+        # ...Next, add a timestamp to the message...
+        sms_str += datetime_str.strftime('(%Y-%m-%d %H:%M:%S): ')
+    
+        # ...Finally, add the actual content of the message...
+        if not message.is_multipart():
+            # (Get rid of that silly '\r' that is used by Windows)
+            sms_str += message.get_payload(decode=True).decode('utf-8').strip().replace('\r', '')
+        else:
+            # ...Unless it was an MMS!
+            for part in message.walk():
+                # ...Save any attachments...
+                if part.get_content_disposition() == 'attachment':
+                    # (...as long as they're not from me, I already have a copy!...)
+                    if not from_me:
+                        sms_str += '[' + part.get_filename() + '] '
+                        with open(datetime_str.strftime('%Y%m%d') + '-' + part.get_filename(), 'wb') as attachment_file:
+                            attachment_file.write(part.get_payload(decode=True))
+                elif part.get_content_type() == 'text/plain':
+                    # ...And save any other text that was included in the message, too...
+                    sms_str += part.get_payload(decode=True).decode('utf-8').strip().replace('\r', '')
+                else:
+                    if part.get_content_type() != 'multipart/mixed':
+                        print('WARNING: Unknown type: ' + part.get_content_type(), file=sys.stderr)
+    
+        # Finally, write (or append) the message to a dated file
+        sms_filename = 'sms-' + datetime_str.strftime('%Y%m') + '-' + timestamp_str + '.txt'
+        with open(sms_filename, 'a') as yearmonth_file:
+            yearmonth_file.write(sms_str.strip() + '\n')
+
+if __name__ == '__main__':
+    message = """
+    SMS Backup+ Backup ("smsbackupbackup")
+    David Couzelis, 2017
+    
+    This file is meant to be used as a library.
+
+    Example:
+
+        #!/usr/bin/env python3
+        import smsbackupbackup
+        import sys
+        smsbackupbackup.run(sys.argv[1], 'John', ['johnemailname', '4565552324'], 'Tony', ['Tony', 'abcmail'])
+
+    Please see the README.md or docstring for more information.
+    """
+    print(message, file=sys.stderr)
+    exit(1)
